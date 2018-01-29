afad27cc5f462b475eabaf3d3e14ef228e964ae6
Parnmet <mos_t_@hotmail.com>
2018-01-29 07:42:14 +0700

    add collectionId

--- build/TwitterConnector/config.js
+++ build/TwitterConnector/config.js
@@ -9,7 +9,7 @@ export const db = mongojs(databaseUrl, collections)
 export const host = 'https://api.smartcity.kmitl.io/api/v1/'
 export const user = 'twitter-connector'
 export const pass =  'TwitterConnector#123'
-export const collectionId = ''
+export const collectionId = '9330e9183d8fadae5aee0854cd4114f0a300c168b36f2dc9904e9b5b239d7dce'
 // import Sequelize from 'sequelize'
 //
 // export const sequelize = new Sequelize('SocialRest', 'rest', 'restapi', {
