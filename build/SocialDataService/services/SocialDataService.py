4f606f91e337ac3848c70148a64295f8bc1a6706
parnmet <mos_t_@hotmail.com>
2018-02-18 13:23:16 +0700

    Fixed  TypeError: if no direction is specified, key_or_list must be an instance of list (_id)

--- build/SocialDataService/services/SocialDataService.py
+++ build/SocialDataService/services/SocialDataService.py
@@ -140,7 +140,7 @@ def getLastestTweets():
         tweet['lat'] = coord[0]
         tweet['lng'] = coord[1]
         # tweet['text'] = tweet_collection.find({"place_id": p['place_id']}, {'text':{'$gt':[]}}).sort({_id:-1}).limit(1)
-        tweet['text'] = tweet_collection.find({"place_id": p['place_id']},{'_id':0,'text':1}).sort({'_id':-1}).limit(1)
+        tweet['text'] = tweet_collection.find({"place_id": p['place_id']},{'text':1}).sort({'_id':-1}).limit(1)
         tweet_list.append(tweet)
     return tweet_list    
 
