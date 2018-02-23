e1ad089010d4b3348fcd640c0cc4061bc9aa8d78
Parnmet <mos_t_@hotmail.com>
2018-02-23 17:12:38 +0700

    Temporary stoped cron that caused program error

--- build/FacebookConnector/service/FacebookService.js
+++ build/FacebookConnector/service/FacebookService.js
@@ -71,143 +71,143 @@ export function getFbUser(userID) {
   })
 }
 
-function saveFbUser(userID) {
-  db.FB_USER.findOne({id: userID}, (err, document) => {
-    if(!document) {
-      getFbUser(userID).then(user => {
-        user.picture = user.picture.url
-        db.FB_USER.insert(user, err => {
-          if(err) {
-            console.log(err)
-          }
-        })
-      })
-    }
-  })
-}
+// function saveFbUser(userID) {
+//   db.FB_USER.findOne({id: userID}, (err, document) => {
+//     if(!document) {
+//       getFbUser(userID).then(user => {
+//         user.picture = user.picture.url
+//         db.FB_USER.insert(user, err => {
+//           if(err) {
+//             console.log(err)
+//           }
+//         })
+//       })
+//     }
+//   })
+// }
 
-function saveFbPage(pageID) {
-  db.FB_PAGE.findOne({id: pageID}, (err, document) => {
-    if(!document) {
-      getFbPageDetail(pageID).then(page => {
-        db.FB_PAGE.insert(page, err => {
-          if(err) {
-            console.log(err)
-          }
-        })
-      })
-    }
-  })
-}
+// function saveFbPage(pageID) {
+//   db.FB_PAGE.findOne({id: pageID}, (err, document) => {
+//     if(!document) {
+//       getFbPageDetail(pageID).then(page => {
+//         db.FB_PAGE.insert(page, err => {
+//           if(err) {
+//             console.log(err)
+//           }
+//         })
+//       })
+//     }
+//   })
+// }
 
-function saveFbPost(pageID) {
-  let until = new Date()
-  let since = new Date()
-  since.setDate(until.getDate() - 5)
-  let untilStr = until.getFullYear() + '-' + (until.getMonth() + 1) + '-' + until.getDate()
-  let sinceStr = since.getFullYear() + '-' + (since.getMonth() + 1) + '-' + since.getDate()
-  getFbFeed(pageID, sinceStr, untilStr).then(feeds => {
-    feeds.data.forEach(feed => {
-      db.FB_POST.findOne({id: feed.id}, (err, document) => {
-        if(!document) {
-          let feedTemp = {}
-          feedTemp.id = feed.id
-          feedTemp.created_time = new Date(feed.created_time)
-          feedTemp.msg = feed.message
-          feedTemp.page_id = pageID
-          db.FB_POST.insert(feedTemp, err => {
-            if(err) {
-              console.log(err)
-            }
-          })
-        }
-      })
-    })
-  })
-}
+// function saveFbPost(pageID) {
+//   let until = new Date()
+//   let since = new Date()
+//   since.setDate(until.getDate() - 5)
+//   let untilStr = until.getFullYear() + '-' + (until.getMonth() + 1) + '-' + until.getDate()
+//   let sinceStr = since.getFullYear() + '-' + (since.getMonth() + 1) + '-' + since.getDate()
+//   getFbFeed(pageID, sinceStr, untilStr).then(feeds => {
+//     feeds.data.forEach(feed => {
+//       db.FB_POST.findOne({id: feed.id}, (err, document) => {
+//         if(!document) {
+//           let feedTemp = {}
+//           feedTemp.id = feed.id
+//           feedTemp.created_time = new Date(feed.created_time)
+//           feedTemp.msg = feed.message
+//           feedTemp.page_id = pageID
+//           db.FB_POST.insert(feedTemp, err => {
+//             if(err) {
+//               console.log(err)
+//             }
+//           })
+//         }
+//       })
+//     })
+//   })
+// }
 
-function saveFbComment(postID) {
-  getFbComment(postID).then(comments => {
-    comments.data.forEach(comment => {
-      db.FB_COMMENT.findOne({id: comment.id}, (err, document) => {
-        if(!document) {
-          comment.post_id = postID
-          comment.created_time = new Date(comment.created_time)
-          db.FB_COMMENT.insert(comment, err=> {
-            if(err) {
-              console.log(err)
-            }
-          })
-        }
-      })
-    })
-  })
-}
+// function saveFbComment(postID) {
+//   getFbComment(postID).then(comments => {
+//     comments.data.forEach(comment => {
+//       db.FB_COMMENT.findOne({id: comment.id}, (err, document) => {
+//         if(!document) {
+//           comment.post_id = postID
+//           comment.created_time = new Date(comment.created_time)
+//           db.FB_COMMENT.insert(comment, err=> {
+//             if(err) {
+//               console.log(err)
+//             }
+//           })
+//         }
+//       })
+//     })
+//   })
+// }
 
-//cron for user
-const cronSaveFbUser = new cronJob('*/2 * * * *', () => {
-  db.FB_COMMENT.find((err, document) => {
-    if(err) console.log(err)
-    document.forEach(comment => {
-      if(comment.from != undefined)
-        saveFbUser(comment.from.id)
-      else
-        saveFbUser(comment.id)
-    })
-    console.log("save user")
-  })
-},
-() => {
-  console.log('cronSaveFbUser has stopped')
-},
-true
-)
+// //cron for user
+// const cronSaveFbUser = new cronJob('*/2 * * * *', () => {
+//   db.FB_COMMENT.find((err, document) => {
+//     if(err) console.log(err)
+//     document.forEach(comment => {
+//       if(comment.from != undefined)
+//         saveFbUser(comment.from.id)
+//       else
+//         saveFbUser(comment.id)
+//     })
+//     console.log("save user")
+//   })
+// },
+// () => {
+//   console.log('cronSaveFbUser has stopped')
+// },
+// true
+// )
 
-//cron for comment
-const cronSaveFbComment = new cronJob('*/2 * * * *', () => {
-  db.FB_POST.find((err, document) => {
-    if(!err) {
-      document.forEach(feed => {
-        saveFbComment(feed.id)
-      })
-    console.log("save comment")
+// //cron for comment
+// const cronSaveFbComment = new cronJob('*/2 * * * *', () => {
+//   db.FB_POST.find((err, document) => {
+//     if(!err) {
+//       document.forEach(feed => {
+//         saveFbComment(feed.id)
+//       })
+//     console.log("save comment")
       
-    }
-    else {
-      console.log(err)
-    }
-  })
-},
-() => {
-  console.log('cronSaveFbComment has stopped')
-},
-true
-)
+//     }
+//     else {
+//       console.log(err)
+//     }
+//   })
+// },
+// () => {
+//   console.log('cronSaveFbComment has stopped')
+// },
+// true
+// )
 
-// cron for post
-const cronSaveFbPost = new cronJob('*/1 * * * *', () => {
-  pageIDs.forEach(pageID => {
-    saveFbPost(pageID)
-  })
-    console.log("save post")
+// // cron for post
+// const cronSaveFbPost = new cronJob('*/1 * * * *', () => {
+//   pageIDs.forEach(pageID => {
+//     saveFbPost(pageID)
+//   })
+//     console.log("save post")
   
-},
-() => {
-  console.log('cronSaveFbPost has stopped.')
-},
-true
-)
+// },
+// () => {
+//   console.log('cronSaveFbPost has stopped.')
+// },
+// true
+// )
 
-//cron for page
-const cronSaveFbPage = new cronJob('*/30 * * * *', () => {
-  pageIDs.forEach(pageID => {
-    saveFbPage(pageID)
-  })
-    console.log("save page")
+// //cron for page
+// const cronSaveFbPage = new cronJob('*/30 * * * *', () => {
+//   pageIDs.forEach(pageID => {
+//     saveFbPage(pageID)
+//   })
+//     console.log("save page")
   
-},
-() => {
-  console.log('cronSaveFbPage has stopped.')
-},
-true
-)
+// },
+// () => {
+//   console.log('cronSaveFbPage has stopped.')
+// },
+// true
+// )
